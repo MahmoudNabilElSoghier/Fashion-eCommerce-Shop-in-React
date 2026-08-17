637e854bbaaf9dba01f51b38d63b10c7593e9750
MahmoudNabilElSoghier <MahmoudNabilElSoghier@gmail.com>
2026-08-17 16:12:54 +0300

    Downgrade plugin-react-swc to stable 3.7.0

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig(({ mode }) => ({
-  plugins: [react()], // بدون أي خيارات إضافية
+  plugins: [react()],
   server: {
     host: "0.0.0.0",
     port: 5173,
